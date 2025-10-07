b4e800affd6ee5908056b6adb14001b03e37305c
kOanhNe <k.oanhlh@gmail.com>
2025-10-07 13:06:14 +0700

    Update MailUtilGmail.java

--- src/main/java/murach/util/MailUtilGmail.java
+++ src/main/java/murach/util/MailUtilGmail.java
@@ -12,13 +12,20 @@ public class MailUtilGmail {
         
         // 1 - get a mail session
         Properties props = new Properties();
-        props.put("mail.transport.protocol", "smtps");
-        props.put("mail.smtps.host", "smtp.gmail.com");
-        props.put("mail.smtps.port", 465);
-        props.put("mail.smtps.auth", "true");
-        props.put("mail.smtps.quitwait", "false");
-        Session session = Session.getDefaultInstance(props);
-        session.setDebug(true);
+        props.put("mail.transport.protocol", "smtp"); // <-- Thay đổi thành "smtp"
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587"); // <-- Thay đổi thành cổng "587"
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true"); // <-- Thêm dòng này
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                // Đọc thông tin từ Biến Môi Trường
+                final String emailUser = System.getenv("GMAIL_USER");
+                final String emailPassword = System.getenv("GMAIL_PASSWORD");
+                return new PasswordAuthentication(emailUser, emailPassword);
+            }
+        });
+        
 
         // 2 - create a message
         Message message = new MimeMessage(session);
